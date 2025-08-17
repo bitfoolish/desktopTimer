00745603c66182b99563f684b400e8bb9bfe6a8c
Shane <shanetraynor21+GH@gmail.com>
2025-08-17 16:03:56 +0100

    removed unneeded loop

--- timer.py
+++ timer.py
@@ -4,8 +4,9 @@ from plyer import notification
 print(time.time())
 
 userTimer = int(input("How many seconds?\n" )) 
-endTime = userTimer + time.time()
-print(time.time(), endTime)
-while (time.time() < (endTime)):
-    time.sleep(1)
-print(f'{time.time()}, time is up you slept for {userTimer} seconds!')
+#endTime = userTimer + time.time()
+#print(time.time(), endTime)
+#while (time.time() < (endTime)):
+print(f'going to sleep for {userTimer} seconds\n  Current time: {time.time()}')
+time.sleep(userTimer)
+print(f'  Current time: {time.time()}, time is up you slept for {userTimer} seconds!')
