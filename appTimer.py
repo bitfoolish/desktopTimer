16085076b19b6c699ee577285e7a058b9342f787
Shane <shanetraynor21+GH@gmail.com>
2025-09-14 20:31:17 +0100

    timer now plays sound upon completion

--- appTimer.py
+++ appTimer.py
@@ -1,20 +1,22 @@
 import tkinter as tk
-import time
+#import time
 from plyer import notification
+from playsound import playsound
 
 class Timer:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.config(bg="#9B3B34")
+        self.window = tk.Tk() 
+        self.window.config(bg="#9B3B34") # background colour for timer app
 
-        self.minsRqst=tk.IntVar()
+        self.minsRqst=tk.IntVar() #
         self.secsRqst=tk.IntVar()
         self.minsRqst.set(0)
         self.secsRqst.set(0)
 
-        self.welcome = tk.Label(text="Please enter your desired time below", fg="black")
+        self.welcome = tk.Label(text="Please enter your desired time below", fg="black") # Welcome message to user at top of program
 
-        self.startButton = tk.Button(
+        # when user clicks button it will begin the timer
+        self.startButton = tk.Button( 
             text="Start Timer",
             width=25,
             height=5,
@@ -23,69 +25,75 @@ class Timer:
             command=self.startTimer
         )
 
-        self.terminateProgram = tk.Button(
+        # user may click this button to exit the program at any time
+        self.terminateProgram = tk.Button( 
             text="Exit program",
             bg="grey",
             fg="yellow",
             command=self.window.destroy
         )
 
-
+        
         self.minsLabel = tk.Label(text="Minutes: ")
-        self.minsEntry = tk.Entry(textvariable= self.minsRqst)
+        self.minsEntry = tk.Entry(textvariable= self.minsRqst) # textbox for user to enter desired minute count, input saved in variable
         self.secsLabel = tk.Label(text="Seconds: ")
-        self.secsEntry = tk.Entry(textvariable=self.secsRqst)
+        self.secsEntry = tk.Entry(textvariable=self.secsRqst) # textbox for user to enter desired minute count, input saved in variable
 
 
-        self.timeRemLabel = tk.Label( text=f"{self.minsRqst.get()}:{self.secsRqst.get()}" )
+        self.timeRemLabel = tk.Label( text=f"{self.minsRqst.get()}:{self.secsRqst.get()}" ) # displays how long is currently left
 
+        # Appears on startup
         self.welcome.pack()
-        self.minsLabel.pack() # Appears on startup
-        self.minsEntry.pack() # Appears on startup, where user enters minute count
-        self.secsLabel.pack() # Appears on startup
-        self.secsEntry.pack() # Appears on startup, where user enters second count
+        self.minsLabel.pack() 
+        self.minsEntry.pack() # where user enters minute count
+        self.secsLabel.pack()
+        self.secsEntry.pack() # where user enters second count
 
         self.timeRemLabel.pack()
-        self.startButton.pack()
+        self.startButton.pack() # user clicks to begin timer
 
         self.terminateProgram.pack() # can click anytime to end program/timer
         self.window.mainloop()
 
-
+    # method to start timer
     def startTimer(self):
         mins=self.minsRqst.get()
         secs=self.secsRqst.get()
 
-        print(f"mins: {self.minsRqst.get()}")
-        print(f"secs: {self.secsRqst.get()}")
+        #print(f"mins: {self.minsRqst.get()}")
+        #print(f"secs: {self.secsRqst.get()}")
 
-        total = ((60*mins) + secs)
-        print(total)
+        total = ((60*mins) + secs) # count how many seconds the timer will take
+        #print(total)
 
-        self.decreaseTimer(self.minsRqst,self.secsRqst,total)
+        self.decreaseTimer(self.minsRqst,self.secsRqst,total) 
 
     def decreaseTimer(self, minsRqst,secsRqst,total):
         if(total == 0): # when timer reaches 0
             self.notify() # notify the user
-            return(self.timeRemLabel.config(text="Time's up"))
+            return(self.timeRemLabel.config(text="Time's up")) 
         
         else:       
-            if((secsRqst.get() == 0) and not (minsRqst == 0)):
-                secsRqst.set(59)
-                minsRqst.set(minsRqst.get() - 1)
+            if((secsRqst.get() == 0) and not (minsRqst == 0)): # E.g. when we reach 1:00 --> 0:59
+                secsRqst.set(59) # second count set to 59
+                minsRqst.set(minsRqst.get() - 1) # decrease minute count by 1
 
             else:
-                secsRqst.set(secsRqst.get() - 1)
+                secsRqst.set(secsRqst.get() - 1) # decrease second count by 1
 
-            self.timeRemLabel.config(text=f"{minsRqst.get():02d}:{secsRqst.get():02d}")
+            self.timeRemLabel.config(text=f"{minsRqst.get():02d}:{secsRqst.get():02d}") # update the label for  
             
-        self.window.after(1000, self.decreaseTimer, minsRqst, secsRqst, total-1) 
+        self.window.after(1000, self.decreaseTimer, minsRqst, secsRqst, total-1) # each second decrease total time remaining by 1
 
     def notify(self): # notifies user timer has ended
         notification.notify(
             title="Timer has ended", 
             message="Want to start again?",
+            app_icon='',
+            app_name='Timer App'
         )
+        playsound('../Soundd-1-1.m4a')
+        playsound('../Soundd-1-1.m4a')
 
 
 if __name__ == "__main__":
