33e68e997c09f4ebe48292fce26943c6d0949cf8
Shane <shanetraynor21+GH@gmail.com>
2025-09-14 16:40:53 +0100

    OOP principles implemented, desktop notify utility added

--- appTimer.py
+++ appTimer.py
@@ -1,103 +1,92 @@
 import tkinter as tk
 import time
+from plyer import notification
 
-window = tk.Tk()
+class Timer:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.config(bg="#9B3B34")
 
-window.config(bg="#9B3B34")
+        self.minsRqst=tk.IntVar()
+        self.secsRqst=tk.IntVar()
+        self.minsRqst.set(0)
+        self.secsRqst.set(0)
+
+        self.welcome = tk.Label(text="Please enter your desired time below", fg="black")
+
+        self.startButton = tk.Button(
+            text="Start Timer",
+            width=25,
+            height=5,
+            bg="blue",
+            fg="yellow",
+            command=self.startTimer
+        )
+
+        self.terminateProgram = tk.Button(
+            text="Exit program",
+            bg="grey",
+            fg="yellow",
+            command=self.window.destroy
+        )
 
 
-minsRqst=tk.IntVar()
-secsRqst=tk.IntVar()
-minsRqst.set(0)
-secsRqst.set(0)
+        self.minsLabel = tk.Label(text="Minutes: ")
+        self.minsEntry = tk.Entry(textvariable= self.minsRqst)
+        self.secsLabel = tk.Label(text="Seconds: ")
+        self.secsEntry = tk.Entry(textvariable=self.secsRqst)
 
 
-def startTimer():
-    mins=minsRqst.get()
-    secs=secsRqst.get()
-    #print(type(secs))
+        self.timeRemLabel = tk.Label( text=f"{self.minsRqst.get()}:{self.secsRqst.get()}" )
+
+        self.welcome.pack()
+        self.minsLabel.pack() # Appears on startup
+        self.minsEntry.pack() # Appears on startup, where user enters minute count
+        self.secsLabel.pack() # Appears on startup
+        self.secsEntry.pack() # Appears on startup, where user enters second count
+
+        self.timeRemLabel.pack()
+        self.startButton.pack()
+
+        self.terminateProgram.pack() # can click anytime to end program/timer
+        self.window.mainloop()
 
 
-    print(f"mins: {minsRqst.get()}")
-    print(f"secs: {secsRqst.get()}")
+    def startTimer(self):
+        mins=self.minsRqst.get()
+        secs=self.secsRqst.get()
 
+        print(f"mins: {self.minsRqst.get()}")
+        print(f"secs: {self.secsRqst.get()}")
 
-    total = ((60*mins) + secs)
-    print(total)
+        total = ((60*mins) + secs)
+        print(total)
 
-    decreaseTimer(minsRqst,secsRqst,total)
+        self.decreaseTimer(self.minsRqst,self.secsRqst,total)
 
-def decreaseTimer(minsRqst,secsRqst,total):
-    
-    if(total == 0): # when timer reaches 0
-        return(timeRemLabel.config(text="Time's up"))
-
-    else:       
-        if((secsRqst.get() == 0) and not (minsRqst == 0)):
-            secsRqst.set(59)
-            minsRqst.set(minsRqst.get() - 1)
-
-        else:
-            secsRqst.set(secsRqst.get() - 1)
-
-        timeRemLabel.config(text=f"{minsRqst.get():02d}:{secsRqst.get():02d}")
+    def decreaseTimer(self, minsRqst,secsRqst,total):
+        if(total == 0): # when timer reaches 0
+            self.notify() # notify the user
+            return(self.timeRemLabel.config(text="Time's up"))
         
-    window.after(1000, decreaseTimer, minsRqst, secsRqst, total-1) 
+        else:       
+            if((secsRqst.get() == 0) and not (minsRqst == 0)):
+                secsRqst.set(59)
+                minsRqst.set(minsRqst.get() - 1)
+
+            else:
+                secsRqst.set(secsRqst.get() - 1)
+
+            self.timeRemLabel.config(text=f"{minsRqst.get():02d}:{secsRqst.get():02d}")
+            
+        self.window.after(1000, self.decreaseTimer, minsRqst, secsRqst, total-1) 
+
+    def notify(self): # notifies user timer has ended
+        notification.notify(
+            title="Timer has ended", 
+            message="Want to start again?",
+        )
 
 
-welcome = tk.Label(
-    text="Please enter your desired time below",
-    fg="black"
-)
-
-startButton = tk.Button(
-    text="Start Timer",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-    command=startTimer
-)
-
-terminateProgram = tk.Button(
-    text="Exit program",
-    bg="grey",
-    fg="yellow",
-    command=window.destroy
-)
-
-
-minsLabel = tk.Label(
-    text="Minutes: "
-)
-
-minsEntry = tk.Entry(
-    textvariable= minsRqst
-)
-
-secsLabel = tk.Label(
-    text="Seconds: "
-)
-
-secsEntry = tk.Entry(
-    textvariable=secsRqst
-)
-
-
-timeRemLabel = tk.Label(
-    text=f"{minsRqst.get()}:{secsRqst.get()}"
-)
-
-welcome.pack()
-
-minsLabel.pack() # Appears on startup
-minsEntry.pack() # Appears on startup, where user enters minute count
-
-secsLabel.pack() # Appears on startup
-secsEntry.pack() # Appears on startup, where user enters second count
-
-timeRemLabel.pack()
-startButton.pack()
-
-terminateProgram.pack() # can click anytime to end program/timer
-window.mainloop()
+if __name__ == "__main__":
+    Timer()
